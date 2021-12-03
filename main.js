909351083e0b48877c650a25282d27477513bad7
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-12-03 15:59:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,49 @@
 function play1(){
     document.getElementById("1").play();
+    document.getElementById("2").pause();
+    document.getElementById("3").pause();
+    document.getElementById("4").pause();
+    document.getElementById("5").pause();
 }
 function pause1(){
     document.getElementById("1").pause();
 }
 function play2(){
     document.getElementById("2").play();
+    document.getElementById("1").pause();
+    document.getElementById("3").pause();
+    document.getElementById("4").pause();
+    document.getElementById("5").pause();
 }
 function pause2(){
     document.getElementById("2").pause();
 }
 function play3(){
     document.getElementById("3").play();
+    document.getElementById("2").pause();
+    document.getElementById("1").pause();
+    document.getElementById("4").pause();
+    document.getElementById("5").pause();
 }
 function pause3(){
     document.getElementById("3").pause();
 }
 function play4(){
     document.getElementById("4").play();
+    document.getElementById("2").pause();
+    document.getElementById("3").pause();
+    document.getElementById("1").pause();
+    document.getElementById("5").pause();
 }
 function pause4(){
     document.getElementById("4").pause();
 }
 function play5(){
     document.getElementById("5").play();
+    document.getElementById("2").pause();
+    document.getElementById("3").pause();
+    document.getElementById("4").pause();
+    document.getElementById("1").pause();
 }
 function pause5(){
     document.getElementById("5").pause();
